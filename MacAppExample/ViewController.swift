b77eadfaea08f83e64a945d02ea69fab7d7b162a
Ave-Wat <wattsa2@carleton.edu>
2021-06-21 14:28:10 -0500

    init

---
+++ MacAppExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MacAppExample
+//
+//  Created by wattsa2 on 6/21/21.
+//  Copyright © 2021 mathcsSysAdmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
